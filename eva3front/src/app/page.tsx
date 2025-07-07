57b45a4a8cfa632a7cdd4022f38e605713676e1c
AUDIBIX <cbbenja@hotmail.com>
2025-07-06 20:14:43 -0400

    Subir
    Se añadió la lógica para subir los datos del formulario al localstorage. Aún no se implementan validaciones

--- eva3front/src/app/page.tsx
+++ eva3front/src/app/page.tsx
@@ -1,15 +1,66 @@
 'use client'
 
+import { useEffect, useState } from "react"
+
+interface Queja{
+  problem_name:string
+  affected:number
+  cat:string
+  desc:string
+  date:Date
+  state:string
+}
+
+const initialStateQueja:Queja = { //Esto le da un valor inicial al state Queja, que se va a ir modificando
+  problem_name:"",
+  affected:1,
+  cat:"maintenance",
+  desc:"",
+  date: new Date(), //Esto crea una fecha con el formato Date y la fecha en la que se ejecutó el programa
+  state:"pending"
+}
+
 export default function Home() {
+
+  const localstorage = window.localStorage
+  const [queja, setqueja] = useState(initialStateQueja)
+  const [quejas, setquejas] = useState<Queja[]>([])
+
+  useEffect(() => {
+    let quejasStr = localstorage.getItem("quejas")
+    if (quejasStr != null){
+      let quejasList = JSON.parse(quejasStr)
+      setquejas(quejasList)
+    }
+  },[])
+
+  const handleQueja = (name:string,value:string|number|Date) => {
+    setqueja({...queja, [name]: value})
+  }
+
+  const handleSubir = () => {
+    localstorage.setItem("quejas", JSON.stringify([...quejas,queja]))
+  }
+
   return (
     <form>
       <h1>Ingreso de quejas</h1>
       <p>Nombre:</p>
-      <input type="text" id="name" placeholder="Plaza sucia"></input><br/>
+      <input 
+      type="text" 
+      name="problem_name" 
+      placeholder="Plaza sucia"
+      onChange={(e)=>{handleQueja(e.currentTarget.name,e.currentTarget.value)}}></input><br/>
       <p>Número de personas afectadas:</p>
-      <input type="number" id="affected" placeholder="Número >= 1"></input><br/>
+      <input 
+      type="number" 
+      name="affected" 
+      placeholder="Número >= 1"
+      onChange={(e)=>{handleQueja(e.currentTarget.name,e.currentTarget.value)}}></input><br/>
       <p>Categoría:</p>
-      <select id="cat">
+      <select 
+      name="cat"
+      onChange={(e)=>{handleQueja(e.currentTarget.name,e.currentTarget.value)}}>
         <option value="maintenance">Mantención</option>
         <option value="security">Seguridad</option>
         <option value="cleaning">Limpieza</option>
@@ -18,16 +69,26 @@ export default function Home() {
         <option value="others">Otro</option>
       </select><br/>
       <p>Descripcion del problema:</p>
-      <input id="desc" type="textarea" maxLength={5000} placeholder="Ej. Alcantarilla rota"></input><br/>
+      <input 
+      name="desc" 
+      type="textarea" 
+      placeholder="Ej. Alcantarilla rota"
+      onChange={(e)=>{handleQueja(e.currentTarget.name,e.currentTarget.value)}}></input><br/>
       <p>Fecha de ingreso de la queja:</p>
-      <input type="date" id="date" placeholder="DD/MM/YYYY"></input><br/>
-      <p>Estado inicial:</p>
-      <select id="state">
+      <input 
+      type="date" 
+      name="date" 
+      placeholder="DD/MM/YYYY"
+      onChange={(e)=>{handleQueja(e.currentTarget.name,e.currentTarget.value)}}></input><br/>
+      <p>Estado:</p>
+      <select 
+      name="state"
+      onChange={(e)=>{handleQueja(e.currentTarget.name,e.currentTarget.value)}}>
         <option value="pending">Pendiente</option>
         <option value="in_progress">En progreso</option>
         <option value="completed">Completada</option>
       </select><br/>
-      <button type="button" id="form_submit">Subir queja</button>
+      <button type="button" name="form_submit" onClick={() => {handleSubir()}} >Subir queja</button>
     </form>
   );
 }
